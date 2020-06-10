f430332bb79ca9324c3670871a7579d2af661de7
LingY060 <ly20170217060>
2020-06-10 10:56:17 +0800

    提交到本次仓库

---
+++ Git-pro/src/com/git/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.git.dao;
+
+import java.awt.List;
+
+public class UserDao {
+	public void addUser(int id,String username,String password) {
+		//添加
+	}
+	
+	public void deleteUser(int id) {
+		//添加
+	}
+	
+	public List getUser(int pageIndex,int pageSize){
+		//获取
+		return null;
+	}
+
+}
